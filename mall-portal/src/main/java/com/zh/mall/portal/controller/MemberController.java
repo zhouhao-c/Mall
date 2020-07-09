2ae68ab47b905847c0b006273d44d4ab0f80117e
zhouhao_global <15623042186@163.com>
2020-07-10 00:02:01 +0800

    文件上传，transferTo方法

--- mall-portal/src/main/java/com/zh/mall/portal/controller/MemberController.java
+++ mall-portal/src/main/java/com/zh/mall/portal/controller/MemberController.java
@@ -1,28 +1,86 @@
 package com.zh.mall.portal.controller;
 
 import com.mall.manager.api.service.GoodsCategoryService;
+import com.mall.manager.api.service.TagService;
+import com.mall.portal.api.service.MemberService;
 import com.zh.mall.common.BaseController;
+import com.zh.mall.common.bean.GoodsCategory;
 import com.zh.mall.common.bean.Member;
+import com.zh.mall.common.bean.Tag;
 import com.zh.mall.common.util.MD5Util;
-import com.zh.mall.portal.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.*;
 
 @Controller
 @RequestMapping("/member")
 public class MemberController extends BaseController {
     @Autowired
     private MemberService memberService;
-
     @Autowired
     private GoodsCategoryService goodsCategoryService;
+    @Autowired
+    private TagService tagService;
+
+    @RequestMapping("/insertGoods")
+    public String insertGoods(HttpServletRequest request) throws Exception{
+        MultipartHttpServletRequest req = (MultipartHttpServletRequest)request;
+        MultipartFile goodsHeadImgFile = req.getFile("goodsHeadImg");
+
+        //获取当前web应用的文件路径
+        ServletContext application = request.getSession().getServletContext();
+        String imgSavaPath = application.getRealPath("static/img/goods");
+
+        //真实文件名称
+        String fileName = goodsHeadImgFile.getOriginalFilename();
+        String uuid = UUID.randomUUID().toString();
+        //UUID.png
+        String suffix = fileName.substring(fileName.lastIndexOf("."));
+        String targetFileName = uuid+suffix;
+        File file = new File(imgSavaPath+"/"+targetFileName);
+
+        //保存文件
+        //装饰者模式
+        //代理模式
+        //new BufferedInputStream(new FileInputStream());
+
+        goodsHeadImgFile.transferTo(file);
+        return "redirect:/member";
+    }
 
     @RequestMapping("/addGoods")
-    public String addGoods(){
+    public String addGoods(Model model){
+        List<GoodsCategory> gcs = goodsCategoryService.queryAll();
+        model.addAttribute("gcs",gcs);
+
+        Tag rootTag = null;
+        List<Tag> allTags = tagService.queryAll();
+        Map<Integer,Tag> tagMap = new HashMap<Integer, Tag>();
+        for (Tag tag:allTags){
+            tagMap.put(tag.getId(),tag);
+        }
+        for (Tag tag:allTags){
+            Tag child = tag;
+            if (child.getPid() == 0){
+                rootTag = tag;
+            }else {
+                Tag parent = tagMap.get(child.getPid());
+                parent.getChildren().add(child);
+            }
+        }
+        model.addAttribute("rootTag",rootTag);
         return "member/addGoods";
     }
 
